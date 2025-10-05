13ff5c2b4e0fbb81976a5cd9c4e882af76dc0aa5
root-tushar <goyal.tushar1974@gmail.com>
2025-10-05 23:49:54 +0530

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MD7V9LVG');`}
+        </Script>
+        
         {/* Google Site Verification */}
         <meta name="google-site-verification" content="pyFff9mkD5EfkPJRU2FRN8hnF0vnR6kchmouVCxn7RE" />
         
@@ -36,6 +45,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MD7V9LVG"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
         {children}
         <SpeedInsights />
       </body>
